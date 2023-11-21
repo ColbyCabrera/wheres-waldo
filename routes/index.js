b3e75be7fca87e42c071bd34278c3c64258ab6f9
ColbyCabrera <gdthyispro@gmail.com>
2023-11-21 13:10:21 -0500

    Add new routes

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ const index_controller = require("../controllers/index");
 /* GET home page. */
 router.get('/', index_controller.index);
 
+router.get('/images', index_controller.images);
+
+router.get('/images/:id', index_controller.get_image);
+
+router.get('/images/:id/targets', index_controller.get_targets);
+
 module.exports = router;
