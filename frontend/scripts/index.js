727d15549e818776e3b24b06f2d066f27e3b8f33
ColbyCabrera <gdthyispro@gmail.com>
2023-11-28 17:11:19 -0500

    Remove commented code

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,17 +1,3 @@
-/*function printMousePos(event) {
-  const header = document.getElementById("header");
-  const defaultWidth = 500;
-  let xCoord = event.pageX;
-  let yCoord = event.pageY - (header.offsetHeight - 1); // subtract one for border
-  let screenWidth = window.innerWidth;
-  const ratio = screenWidth / defaultWidth;
-
-  xCoord = Math.floor(xCoord / ratio / 25.0);
-  yCoord = Math.floor(yCoord / ratio / 25.0);
-
-  console.log(xCoord + " " + yCoord);
-}
-
 function toggleMenu(event) {
   let menu = document.getElementById("dropdown-menu");
   menu.classList.toggle("invisible");
@@ -20,8 +6,9 @@ function toggleMenu(event) {
 
   // CHANGE THIS TO BE A DROPDOWN FROM TOP OF SCREEN
 }
-*/
 
+
+// ADD ability to dynamically determine fetch correct data based on what option is clicked
 async function selectCharacter(event) {
   const header = document.getElementById("header");
   const defaultWidth = 500;
@@ -41,6 +28,7 @@ async function selectCharacter(event) {
     let data = await response.json();
     let correctX = data[1][0];
     let correctY = data[1][1];
+
     if (xCoord == correctX && yCoord == correctY) {
       console.log("SUCCESS!");
     }
@@ -61,14 +49,6 @@ async function getImage() {
     console.log(error);
   }
 }
-
-/*
-step 1: get mouse coords from click
-step 2: get ratio with curretnscreensize/defaultscreensize
-step 3: coords/ratio (make sure to subtract height of header)
-step 4: divide coords by 25
-step 5: see if coords match given coords
-*/
 
 let image = document.getElementById("main-image");
 image.addEventListener("click", selectCharacter);
