c3740c3673e458bb02cd0c0ea11c83abcb422b10
ColbyCabrera <gdthyispro@gmail.com>
2023-11-30 15:10:19 -0500

    Change to dynamically fetching target data based on current image

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,3 +1,5 @@
+// Add dom cache?
+
 function toggleMenu(event) {
   let menu = document.getElementById("dropdown-menu");
   menu.classList.toggle("invisible");
@@ -7,50 +9,68 @@ function toggleMenu(event) {
   // CHANGE THIS TO BE A DROPDOWN FROM TOP OF SCREEN
 }
 
-// ADD ability to dynamically determine fetch correct data based on what option is clicked
 async function selectCharacter(event) {
+  const imageNumber = document.getElementById("main-image").dataset.imageNumber;
   const header = document.getElementById("header");
+  const id = event.target.id.slice(7);
   const defaultWidth = 500;
   let xCoord = event.pageX;
   let yCoord = event.pageY - (header.offsetHeight - 1); // subtract one for border
-  let screenWidth = window.innerWidth;
+  const screenWidth = window.innerWidth;
   const ratio = screenWidth / defaultWidth;
+
+  // if id is not one of the options exit function
+  if (isNaN(id)) {
+    return;
+  }
 
   xCoord = Math.floor(xCoord / ratio / 25.0);
   yCoord = Math.floor(yCoord / ratio / 25.0);
 
   try {
-    const response = await fetch("http://localhost:3000/image/1/targets", {
-      mode: "cors",
-    });
+    const response = await fetch(
+      "http://localhost:3000/image/" + imageNumber + "/targets",
+      {
+        mode: "cors",
+      }
+    );
 
     let data = await response.json();
-    let correctX = data[1][0];
-    let correctY = data[1][1];
+    let correctX = data[id][0];
+    let correctY = data[id][1];
 
     if (xCoord == correctX && yCoord == correctY) {
       console.log("SUCCESS!");
     }
+
+    console.log(xCoord);
+    console.log(yCoord);
   } catch (error) {
     console.log(error);
   }
 }
 
+//update to dynamically get images based on user selection
 async function getImage() {
   try {
-    const response = await fetch("http://localhost:3000/image/2", {
+    const image = document.getElementById("main-image");
+    const imageNumber = 2;
+    const response = await fetch("http://localhost:3000/image/" + imageNumber, {
       mode: "cors",
     });
     const blob = await response.blob();
     const imageUrl = URL.createObjectURL(blob);
-    document.getElementById("main-image").src = imageUrl;
+    image.src = imageUrl;
+    image.dataset.imageNumber = imageNumber;
   } catch (error) {
     console.log(error);
   }
 }
 
 let image = document.getElementById("main-image");
-image.addEventListener("click", selectCharacter);
+let menu = document.getElementById("dropdown-menu");
+//image.addEventListener("click", selectCharacter);
+menu.addEventListener("click", selectCharacter);
 document.addEventListener("click", toggleMenu);
 
 getImage();
